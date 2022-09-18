e7ce4fec5036eced224dec8158bdde8bcb4da899
Amol Jagtap <amoljagtap17@gmail.com>
2022-09-18 14:23:02 +0530

    react-mechanic-problem1: seed data for seats

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,10 @@ async function main() {
   await prisma.$connect();
 
   await prisma.user.deleteMany();
+  await prisma.building.deleteMany();
+  await prisma.floor.deleteMany();
+  await prisma.wing.deleteMany();
+  await prisma.seat.deleteMany();
 
   for (let i = 0; i < 10; i++) {
     await prisma.user.create({
@@ -18,6 +22,43 @@ async function main() {
         name: faker.name.fullName(),
       },
     });
+  }
+
+  for (let i = 1; i < 2; i++) {
+    const building = await prisma.building.create({
+      data: {
+        buildingName: `building-${i}`,
+      },
+    });
+
+    for (let i = 1; i < 5; i++) {
+      const floor = await prisma.floor.create({
+        data: {
+          buildingId: building.id,
+          floorNo: i,
+        },
+      });
+
+      for (let i = 1; i < 4; i++) {
+        const wingName = i === 1 ? "A" : i === 2 ? "B" : i === 3 ? "C" : "D";
+
+        const wing = await prisma.wing.create({
+          data: {
+            floorId: floor.id,
+            wingName: `L${floor.floorNo}-${wingName}`,
+          },
+        });
+
+        // 54 seats per wing
+        for (let i = 1; i < 55; i++) {
+          await prisma.seat.create({
+            data: {
+              seatNumber: `${wing.wingName}-${i.toString().padStart(3, "0")}`,
+            },
+          });
+        }
+      }
+    }
   }
 }
 
